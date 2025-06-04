d902427ead4730120b3dd9d2f55cf1621f820642
Dipen Desai <desadipe@amazon.com>
2025-06-04 13:45:05 -0500

    Updating files to work with isen account

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "kunduso-terraform-remote-bucket"
+    bucket  = "ecs-bg-deploy-gallup-20250602"
     encrypt = true
     key     = "tf/add-aws-ecr-ecs-fargate/deploy-ecs.tfstate"
-    region  = "us-east-2"
+    region  = "us-east-1"
   }
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,7 +2,7 @@
 variable "region" {
   description = "Infrastructure region."
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 #Define IAM User Access Key
 variable "access_key" {
@@ -21,7 +21,7 @@ variable "secret_key" {
 variable "name" {
   description = "The name of the application."
   type        = string
-  default     = "app-6"
+  default     = "ecs-bg-test"
 }
 variable "vpc_cidr" {
   description = "The CIDR of the VPC."
@@ -41,7 +41,7 @@ variable "subnet_cidr_private" {
 variable "availability_zone" {
   description = "The availability zones for teh public subnets."
   type        = list(any)
-  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "ecs_secret" {
