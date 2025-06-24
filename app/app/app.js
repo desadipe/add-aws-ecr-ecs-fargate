d105fa3b77581f20aaec2ad9832f74e8e7c6e034
Dipen Desai <desadipe@amazon.com>
2025-06-24 18:07:20 -0500

    Updating application and testing Webhooks

--- app/app/app.js
+++ app/app/app.js
@@ -13,7 +13,7 @@ app.use(morgan('combined'));
 app.use('/healthcheck', require('./route/healthcheck'));
 
 const port = process.env.PORT || 8080;
-const message = process.env.MESSAGE || "Hello from the Docker container! Hosted on ECS - Test on 20250623";
+const message = process.env.MESSAGE || "Hello from the Docker container! Hosted on ECS - Test on 20250624-1";
 const secretName = 'ecs_secret'; // Name of your AWS Secrets Manager secret
 
 // Retrieve secret value asynchronously
