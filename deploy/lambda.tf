e8c3df4dbfd9d6e75d91337e95d5ae3318b984d2
Dipen Desai <desadipe@amazon.com>
2025-06-27 15:54:49 -0500

    20250627 - Adding TF files for SNS, Lambda and StateMachine - 4

--- deploy/lambda.tf
+++ deploy/lambda.tf
@@ -2,7 +2,7 @@
 resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function.zip"
   function_name = "ecs-bg-lambda-test-tf"
-  role          = aws_iam_role.lambda_role.arn
+  role          = "arn:aws:iam::791573251752:role/dd-lambdaSSMFullAccess-Role"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.13"
 
