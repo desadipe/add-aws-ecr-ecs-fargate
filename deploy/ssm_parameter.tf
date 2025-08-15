55445784c2289568c9dcad055b28d6a88e61602c
Dipen Desai <desadipe@amazon.com>
2025-08-15 14:44:48 -0500

    20250815 - Updating Hooks for ECS update-service command --- 4

---
+++ deploy/ssm_parameter.tf
@@ -0,0 +1,12 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ssm_parameter
+resource "aws_ssm_parameter" "state_machine_info" {
+  name        = "/${aws_sfn_state_machine.sfn_state_machine.name}/info"
+  description = "State Machine Execution Information"
+  type        = "String"
+  value       = ""
+  lifecycle {
+    ignore_changes = [value]
+  }
+
+  #checkov:skip=CKV2_AWS_34: "AWS SSM Parameter should be Encrypted"
+}
