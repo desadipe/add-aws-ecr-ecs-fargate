e273521f8297edaf981e13caee14da1ebe232726
Dipen Desai <desadipe@amazon.com>
2025-08-15 15:01:02 -0500

    20250815 - Updating Hooks for ECS update-service command --- 5

--- deploy/ssm_parameter.tf
+++ deploy/ssm_parameter.tf
@@ -1,6 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ssm_parameter
 resource "aws_ssm_parameter" "state_machine_info" {
-  name        = "/${aws_sfn_state_machine.sfn_state_machine.name}/info"
+  # name        = "/${aws_sfn_state_machine.sfn_state_machine.name}/info"
+  name        = "/ecs-bg-test-state-machine/info"
   description = "State Machine Execution Information"
   type        = "String"
   value       = ""
